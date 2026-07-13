d285d915f2ff1ae729d552fa09a697344d90e6ec
Szentkatolnai Gábor <szega77@hotmail.com>
2026-07-13 22:29:02 +0200

    Fix Purchase price form color select

--- app/Filament/Resources/ProductPurchasePrices/Schemas/ProductPurchasePriceForm.php
+++ app/Filament/Resources/ProductPurchasePrices/Schemas/ProductPurchasePriceForm.php
@@ -9,7 +9,6 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
-use App\Models\Color;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Utilities\Set;
 
