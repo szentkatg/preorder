bb1f40fe437bdca79e952a33d835ec90cb459439
Szentkatolnai Gábor <szega77@hotmail.com>
2026-07-16 12:29:40 +0200

    Restore collection import in stock proportioning

--- app/Services/Orders/StockOrderProportioningService.php
+++ app/Services/Orders/StockOrderProportioningService.php
@@ -4,6 +4,7 @@ namespace App\Services\Orders;
 
 use App\Models\Order;
 use App\Models\OrderItem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use InvalidArgumentException;
 
